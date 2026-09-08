db9b4a9cdbdc4765bc02967b9067480a5ace07fa
Anna <goryajnovaahstep@gmail.com>
2026-09-08 20:59:43 +0300

    Initial Commit

---
+++ Fitness API/Fitness_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_APIApp.swift
+//  Fitness API
+//
+//  Created by Анна Горяйнова on 08.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
